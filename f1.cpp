7a48feabd00d70cda6bc820099b47afe482a6298
Kunwar-13 <ayushpreetsingh20@gmail.com>
2024-05-17 12:38:14 -0400

    Taking input from the user

---
+++ f1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int numberOfPairs = 10;
+	int maxNumberOfCharacters = 30; 
+	int i = 0; 
+	char nameOfTheDestination[numberOfPairs][maxNumberOfCharacters];
+	char dateOfFlight[numberOfPairs][maxNumberOfCharacters];
+
+	for (i = 0; i < numberOfPairs; i++) {
+		printf("Please enter the name of the destination: ");
+		fgets(nameOfTheDestination[i], maxNumberOfCharacters, stdin);
+		printf("\nPlease enter the date of your flight(DD/MM/YYYY): ");
+		fgets(dateOfFlight[i], maxNumberOfCharacters, stdin);
+	}
+
+	return 0;
+}
