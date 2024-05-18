4c30d0683aff3081495d1afb373996edd58ea4b4
Kunwar-13 <ayushpreetsingh20@gmail.com>
2024-05-17 21:51:37 -0400

    fillFlightInfo function

--- f1.cpp
+++ f1.cpp
@@ -8,20 +8,36 @@ struct FlightInfo {
 	char* date;
 };
 
+void fillFlightInfo(*flights, char array1[], char array2[]);
+
 int main(void) {
 
 	int numberOfPairs = 10;
 	int maxNumberOfCharacters = 30; 
 	int i = 0; 
-	char nameOfTheDestination[numberOfPairs][maxNumberOfCharacters];
-	char dateOfFlight[numberOfPairs][maxNumberOfCharacters];
+	char nameOfTheDestination[maxNumberOfCharacters];
+	char dateOfFlight[maxNumberOfCharacters];
+	struct FlightInfo flights[numberOfPairs];
 
 	for (i = 0; i < numberOfPairs; i++) {
 		printf("Please enter the name of the destination: ");
-		fgets(nameOfTheDestination[i], maxNumberOfCharacters, stdin);
+		fgets(nameOfTheDestination, maxNumberOfCharacters, stdin);
 		printf("\nPlease enter the date of your flight(DD/MM/YYYY): ");
-		fgets(dateOfFlight[i], maxNumberOfCharacters, stdin);
+		fgets(dateOfFlight, maxNumberOfCharacters, stdin);
+
+		fillFlightInfo(&flights[i], nameOfTheDestination, dateOfFlight);
 	}
 
+	
+
 	return 0;
+}
+
+void fillFlightInfo(*flights, char array1[], char array2[]) {
+
+	flights->destination = malloc((strlen(array1) + 1) * sizeof(char));
+	strcpy(flights->destination, array1);
+
+	flights->date = malloc((strlen(array2) + 1) * sizeof(char));
+	strcpy(flights->date , array2);
 }
